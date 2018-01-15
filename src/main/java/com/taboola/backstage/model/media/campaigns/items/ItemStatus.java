726a048b55811eb5fcce3b70d7db1984b3fb5e4c
vadimmanaev <vadimmanaev@gmail.com>
2018-01-15 22:55:22 +0200

    Added Javadoc for ItemStatus and ItemType

--- src/main/java/com/taboola/backstage/model/media/campaigns/items/ItemStatus.java
+++ src/main/java/com/taboola/backstage/model/media/campaigns/items/ItemStatus.java
@@ -1,6 +1,8 @@
 package com.taboola.backstage.model.media.campaigns.items;
 
 /**
+ * Items with status=RUNNING is the only item that is being served in Taboola Widgets.
+ *
  * Created by vladi
  * Date: 12/23/2017
  * Time: 10:31 PM
--- src/main/java/com/taboola/backstage/model/media/campaigns/items/ItemType.java
+++ src/main/java/com/taboola/backstage/model/media/campaigns/items/ItemType.java
@@ -1,6 +1,12 @@
 package com.taboola.backstage.model.media.campaigns.items;
 
 /**
+ * <p>
+ *    There are two possible types of items:
+ *    <br>⚫ ITEM - A simple Item, referencing a specific URL of a web page (e.g. link to an article).
+ *    <br>⚫ RSS - An item with a URL referencing an RSS feed (we support mRSS format). This Item will have Children Items - an Item for each URL in its feed.
+ * </p>
+ *
  * Created by vladi
  * Date: 1/15/2018
  * Time: 10:03 PM
