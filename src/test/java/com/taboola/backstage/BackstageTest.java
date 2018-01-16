7af1c32ec952858eca0c6c03d662329954763c00
vadimmanaev <vadimmanaev@gmail.com>
2018-01-16 22:36:28 +0200

    Added happy flow test for Backstage

---
+++ src/test/java/com/taboola/backstage/BackstageTest.java
@@ -0,0 +1,25 @@
+package com.taboola.backstage;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by vladi
+ * Date: 1/16/2018
+ * Time: 9:54 PM
+ * By Taboola
+ */
+public class BackstageTest extends BackstageTestBase {
+
+    @Test
+    public void testHappyFlowServices() {
+        Backstage testInstance = Backstage.builder().build();
+        Assert.assertNotNull("Missing service instance", testInstance.accountsService());
+        Assert.assertNotNull("Missing service instance", testInstance.authenticationService());
+        Assert.assertNotNull("Missing service instance", testInstance.campaignItemsService());
+        Assert.assertNotNull("Missing service instance", testInstance.campaignPostalCodeTargetingService());
+        Assert.assertNotNull("Missing service instance", testInstance.campaignsService());
+        Assert.assertNotNull("Missing service instance", testInstance.dictionaryService());
+        Assert.assertNotNull("Missing service instance", testInstance.reportsService());
+    }
+}
