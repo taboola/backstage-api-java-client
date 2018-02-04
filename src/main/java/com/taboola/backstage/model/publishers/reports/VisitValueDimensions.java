470f58e2c51ce5cad04f50af7109ed12f2d77ebb
vadimmanaev <vadimmanaev@gmail.com>
2018-02-04 22:31:23 +0200

    Completed visit value row structure with dimensions and optional filters

--- src/main/java/com/taboola/backstage/model/publishers/reports/VisitValueDimensions.java
+++ src/main/java/com/taboola/backstage/model/publishers/reports/VisitValueDimensions.java
@@ -10,8 +10,17 @@ public enum VisitValueDimensions {
 
     DAY("day"),
     WEEK("week"),
-    MONTH("month"),;
-    //TODO impl
+    MONTH("month"),
+    BY_REFERRAL("by_referral"),
+    LANDING_PAGE_BREAKDOWN("landing_page_breakdown"),
+    PLATFORM_BREAKDOWN("platform_breakdown"),
+    COUNTRY_BREAKDOWN("country_breakdown"),
+    PAGE_TYPE_BREAKDOWN("page_type_breakdown"),
+    DAY_REFERRAL_LANDING_PAGE_BREAKDOWN("day_referral_landing_page_breakdown"),
+    BY_SOURCE_MEDIUM("by_source_medium"),
+    BY_CAMPAIGN("by_campaign"),
+    BY_CUSTOM_TRACKING_CODE("by_custom_tracking_code"),
+    BY_REFERRAL_AND_TRACKING_CODE("by_referral_and_tracking_code");
 
     private final String name;
 
--- src/main/java/com/taboola/backstage/model/publishers/reports/VisitValueOptionalFilters.java
+++ src/main/java/com/taboola/backstage/model/publishers/reports/VisitValueOptionalFilters.java
@@ -8,12 +8,29 @@ import com.taboola.backstage.model.ReportFilter;
  * Time: 10:48 PM
  * By Taboola
  */
-public class VisitValueOptionalFilters implements ReportFilter {
+public enum VisitValueOptionalFilters implements ReportFilter {
 
-    //TODO impl
+    REFERRAL_DOMAIN("referral_domain"),
+    LANDING_PAGE("landing_page"),
+    COUNTRY("country"),
+    PLATFORM("platform"),
+    CAMPAIGN_SOURCE("campaign_source"),
+    CAMPAIGN_MEDIUM("campaign_medium"),
+    CAMPAIGN_TERM("campaign_term"),
+    CAMPAIGN_CONTENT("campaign_content"),
+    CAMPAIGN_NAME("campaign_name"),
+    CUSTOM_KEY("custom_key"),
+    CUSTOM_VALUE("custom_value"),
+    PAGE_TYPE("page_type");
+
+    private final String name;
+
+    VisitValueOptionalFilters(String name) {
+        this.name = name;
+    }
 
     @Override
     public String getName() {
-        return null;
+        return name;
     }
 }
