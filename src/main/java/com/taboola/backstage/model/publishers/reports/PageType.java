bd343734207c206b333982cc9072e98889f7510d
vadimmanaev <vadimmanaev@gmail.com>
2018-02-04 23:33:06 +0200

    Added publisher platform and page type enumerations

---
+++ src/main/java/com/taboola/backstage/model/publishers/reports/PageType.java
@@ -0,0 +1,17 @@
+package com.taboola.backstage.model.publishers.reports;
+
+/**
+ * Created by vladi
+ * Date: 2/4/2018
+ * Time: 11:04 PM
+ * By Taboola
+ */
+public enum PageType {
+    TEXT, //Story Pages
+    VIDEO, //Video Pages
+    PHOTO, //Photo Galleries
+    CATEGORY, //Section Fronts
+    HOMEPAGE, //Homepage
+    SEARCH, //Search Result Pages
+    OTHER //Other Pages
+}
