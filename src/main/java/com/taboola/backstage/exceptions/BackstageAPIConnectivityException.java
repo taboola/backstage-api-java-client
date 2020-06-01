9a366f274f511e3d858e56eb8b6e6e60f44a4586
vladimanaev <vadimmanaev@gmail.com>
2020-06-01 12:08:50 +0300

    Added error cause from API response and factory layer

--- src/main/java/com/taboola/backstage/exceptions/BackstageAPIConnectivityException.java
+++ src/main/java/com/taboola/backstage/exceptions/BackstageAPIConnectivityException.java
@@ -9,11 +9,22 @@ package com.taboola.backstage.exceptions;
  */
 public class BackstageAPIConnectivityException extends BackstageAPIException {
 
+    private static final String ERROR_STR = "Failed to perform API call, might be due to internet connectivity issues";
+    private static final String ERROR_STR_WITH_RESPONSE_CODE = "Failed to perform API call with response code [%d], might be due to internet connectivity issues";
+
     public BackstageAPIConnectivityException() {
-        super("Failed to perform API call, might be due to internet connectivity issues");
+        super(ERROR_STR);
+    }
+
+    public BackstageAPIConnectivityException(Throwable cause) {
+        super(cause, ERROR_STR);
     }
 
     public BackstageAPIConnectivityException(int responseCode) {
-        super("Failed to perform API call with response code [%d], might be due to internet connectivity issues", responseCode);
+        super(ERROR_STR_WITH_RESPONSE_CODE, responseCode);
+    }
+
+    public BackstageAPIConnectivityException(Throwable cause, int responseCode) {
+        super(cause, ERROR_STR_WITH_RESPONSE_CODE, responseCode);
     }
 }
