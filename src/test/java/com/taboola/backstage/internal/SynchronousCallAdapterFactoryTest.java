fde2dad968d3eb1d3c7ea6c59404c200ba64d1ae
Assaf Kochavi <assaf.k@taboola.com>
2020-10-12 13:41:34 +0300

    fix tests

--- src/test/java/com/taboola/backstage/internal/SynchronousCallAdapterFactoryTest.java
+++ src/test/java/com/taboola/backstage/internal/SynchronousCallAdapterFactoryTest.java
@@ -55,7 +55,7 @@ public class SynchronousCallAdapterFactoryTest extends BackstageTestBase {
         apiError = testInstance.normalizeError(apiError);
         BackstageAPIRequestException ex = new BackstageAPIRequestException(400, apiError);
         logger.info(ex);
-        assertEquals("Failed to perform API call with response code [400]. Response payload status [400], message [Cpc boost value must be between -99% and 100%], offending field [null]", ex.getMessage());
+        assertEquals("Failed to perform API call with response code [400]. Response payload status [400], message [Cpc boost value must be between -99% and 100%], offending field [null], message code [null]", ex.getMessage());
     }
 
     //TODO add more tests - retrofit2#Response.class is final therefore mockito is not working on it
