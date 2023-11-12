4ba6f230ecd958fbbe042f45dd0ea15a325a11dc
vladimanaev <vadimmanaev@gmail.com>
2023-11-12 23:00:26 +0200

    Adding more bid strategies

--- src/main/java/com/taboola/backstage/model/media/campaigns/CampaignBidStrategy.java
+++ src/main/java/com/taboola/backstage/model/media/campaigns/CampaignBidStrategy.java
@@ -1,5 +1,7 @@
 package com.taboola.backstage.model.media.campaigns;
 
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
+
 /**
  * Created by vladi.m
  * Date 16/04/2020
@@ -8,5 +10,8 @@ package com.taboola.backstage.model.media.campaigns;
  */
 public enum CampaignBidStrategy {
     SMART,
-    FIXED
+    FIXED,
+    TARGET_CPA,
+    MAX_CONVERSIONS,
+    @JsonEnumDefaultValue UNSUPPORTED_BY_SDK_VALUE
 }
